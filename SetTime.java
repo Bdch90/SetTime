be45f41692bf6a53c3337e4d83f2a281bdc5b1e1
Bdch90 <46187905+Bdch90@users.noreply.github.com>
2019-02-28 11:00:02 +0700

    Create SetTime.java

---
+++ SetTime.java
@@ -0,0 +1,37 @@
+package StopWatch;
+import java.text.ParseException;
+import java.util.Scanner;
+
+public class SetTime {
+    public static void main(String[] args) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr;
+        arr = new int[] {4,3,6,34,5,63,7,48,4,3,5,5,3,8,3,35,0,8,8,777,2,2,3,2,3,2,3,23,2,3,2,32,2,4,2,42} ;
+        sort(arr);
+    }
+
+    //phuong thuc tinh time
+    private static void sort(int[] arr)
+    {
+        StopWatch stopWatch = new StopWatch();
+        System.out.println(stopWatch.getStarDay());
+        long start = stopWatch.getStartTime();
+        stopWatch.getStartTime();
+        int min = arr[0];
+        for (int x = 1; x < arr.length; x++)
+        {
+            if(min > arr[x])
+            {
+                min = arr[x];
+            }
+        }
+        for ( int x = 0; x < arr.length; x++)
+        {
+            System.out.print(arr[x] + ".");
+        }
+        System.out.println(stopWatch.getStopDate());
+        long stop = stopWatch.getStopTime();
+        stopWatch.getStopTime();
+        System.out.println(stop-start);
+    }
+}
